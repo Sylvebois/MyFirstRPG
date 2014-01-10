067141d8e68dd1d297d77516ffa4765c635e322c
Sylvebois <thomasghaye@hotmail.com>
2014-01-10 14:49:57 +0100

    Debugged fog

--- public_html/js/generateFog.js
+++ public_html/js/generateFog.js
@@ -32,50 +32,45 @@ var fog = [
 delFog(posHero, heroVis);
 
 //On supprime en fonction de l'emplacement et du champ de vision
-function delFog(coord, champ) {
-    var cmpSemi = 0;
-    var cmpClear = 0;
-    
+function delFog(coord, champ) {            
     //Les cases decouvertes
-    for(var x = coord[0]-champ; x <= coord[0]+champ; x++){		
-        var y1 = coord[1]-cmpClear;
-        var y2 = coord[1]+cmpClear;
+    for(var tmp = champ; tmp >= 0; tmp --) {
+        var cmpClear = 0;
         
-        (x < coord[0]) ? cmpClear++ : cmpClear--;
-        if(x >=0 && y1 >= 0){
-            fog[y1][x] = 2;
-        }
-        if(x >=0 && y2 >= 0){
-            fog[y2][x] = 2;
-        }
-        
-        fcxt.clearRect(x*TILESIZE, y1*TILESIZE, TILESIZE, TILESIZE);
-        fcxt.clearRect(x*TILESIZE, y2*TILESIZE, TILESIZE, TILESIZE);        
-    }
-    for(var y = coord[1]-champ+1; y <= coord[1]+champ-1; y++) { 
-        for(var x = coord[0]-champ+1; x <= coord[0]+champ-1; x++) {  
-            fcxt.clearRect(x*TILESIZE, y*TILESIZE, TILESIZE, TILESIZE);
-            if(x >= 0 && y >= 0) {
-                fog[y][x] = 2;
+        for(var x = coord[0]-tmp; x <= coord[0]+tmp; x++){		
+            var y1 = coord[1]-cmpClear;
+            var y2 = coord[1]+cmpClear;
+
+            (x < coord[0]) ? cmpClear++ : cmpClear--;
+            if(x >=0 && y1 >= 0 && y1 < ROWTILECOUNT){
+                fog[y1][x] = 2;
+                fcxt.clearRect(x*TILESIZE, y1*TILESIZE, TILESIZE, TILESIZE);
             }
+            if(x >=0 && y2 >= 0 && y2 < ROWTILECOUNT){
+                fog[y2][x] = 2;
+                fcxt.clearRect(x*TILESIZE, y2*TILESIZE, TILESIZE, TILESIZE); 
+            }          
         }
     }
     
     //Les cases semi-transparentes
-    fcxt.fillStyle = 'rgba(0, 0, 0, 0.5)';			
+    var cmpSemi = 0;
+    
+    fcxt.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    
     for(var x = coord[0]-champ-1; x <= coord[0]+champ+1; x++){		
         var y1 = coord[1]-cmpSemi;
         var y2 = coord[1]+cmpSemi;
         
         (x < coord[0]) ? cmpSemi++ : cmpSemi--;
         
-        if(x >=0 && y1 >= 0) {
+        if(x >=0 && y1 >= 0 && y1 < ROWTILECOUNT) {
             fog[y1][x] = 1;
             fcxt.clearRect(x*TILESIZE, y1*TILESIZE, TILESIZE, TILESIZE);
             fcxt.fillRect(x*TILESIZE, y1*TILESIZE, TILESIZE, TILESIZE);
         }
         
-        if(x >=0 && y2 >= 0) {
+        if(x >=0 && y2 >= 0 && y2 < ROWTILECOUNT) {
             fog[y2][x] = 1;
             fcxt.clearRect(x*TILESIZE, y2*TILESIZE, TILESIZE, TILESIZE);
             fcxt.fillRect(x*TILESIZE, y2*TILESIZE, TILESIZE, TILESIZE);
