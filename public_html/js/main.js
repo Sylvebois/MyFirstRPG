13c42989093423f96d9f78c807a0167c1c20c281
Sylvebois <thomasghaye@hotmail.com>
2016-06-17 12:02:52 +0200

    Created a function to draw the whole map and a test function for animation

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
     setCanvasSize();
    
     //ajuste la scène si l'écran change de taille
@@ -6,9 +6,11 @@ $(function(){
         setCanvasSize();
     });
 
-    $('canvas').hide();
-    $('#map').show();
+    $('#ui').hide();
+
     startGame();
+    drawWholeMap();
+    move(0,0);
 });
 
 /*
@@ -16,10 +18,10 @@ $(function(){
  */
 function setCanvasSize() {
     var canvasSize = Math.min(window.innerWidth, window.innerHeight);
-
+    
     $('canvas').prop({ width: canvasSize, height: canvasSize });
     
-    tileSizeOnScreen = Math.floor(canvasSize*5/100);
+    tileSizeOnScreen = Math.floor(canvasSize*percentOfScreen);
     sizeOfCanvas = canvasSize;
 }
 
@@ -27,20 +29,65 @@ function setCanvasSize() {
  * Ecran d'accueil du jeu
  */
 function startGame() {
-    var start = document.getElementById('map');
-    var startContext = start.getContext('2d');
-
-//    var background = new Image();
-//    background.src = 'img/scroll.png';
-//    startContext.drawImage(background, 0, 0, background.width, background.height, 0, 0, sizeOfCanvas, sizeOfCanvas);
-    
-    groundImg.onload = function() {
-        for(var i = 0; i < sizeOfCanvas/tileSizeOnScreen; i++) {
-            for(var j = 0; j < sizeOfCanvas/tileSizeOnScreen; j++) {
-                var test = new MapTile(i, j);
-                test.setType();
-                test.draw(startContext, groundImg);
-            }
+    for(var i = 0; i < (sizeOfCanvas/tileSizeOnScreen)-1; i++) {
+        var testPerso = new Perso(i,0);
+        mapTab.push([]);
+        
+        for(var j = 0; j < (sizeOfCanvas/tileSizeOnScreen)-1; j++) {
+            var test = new MapTile(i, j);
+            
+            mapTab[i].push({
+                'sol': test,
+                'fog': 0,
+                'item': 0,
+                'monstre': testPerso,
+                'hero': 0,
+            });
         }
     }
+}
+
+/*
+ * Dessine la carte et ses éléments (sol, murs, items, monstres, héros ...)
+ */
+function drawWholeMap() {
+    for(var i in mapTab) {
+        for(var j in mapTab[i]) {
+            mapTab[i][j].sol.draw(mapContext, groundImg);
+            
+            (mapTab[i][j].item !== 0)? mapTab[i][j].item.draw(mapContext, itemsImg) : null;
+            (mapTab[i][j].monstre !== 0)? mapTab[i][j].monstre.draw(mapContext, monstersImg) : null;
+            (mapTab[i][j].hero !== 0)? mapTab[i][j].hero.draw(mapContext, herosImg) : null;
+           
+            //mapTab[i][j].fog;
+        }
+    }
+}
+
+
+//Test de déplacement et de rafraichissement du canvas
+function move(x, y) {
+    if(y > mapTab[0].length-2) {
+        y = 0;
+        x++;
+    }
+    if(x > mapTab.length-1) {
+        return 0;
+    }
+    
+    var start = document.getElementById('map');
+    var startContext = start.getContext('2d');
+    
+    var tmp = mapTab[x][y].monstre;
+    tmp.setPos(x,y+1);
+
+    mapTab[x][y].monstre = 0;
+    mapTab[x][y].sol.draw(startContext, groundImg);
+
+    mapTab[x][y+1].monstre = tmp;
+    mapTab[x][y+1].monstre.draw(startContext, monstersImg);
+    
+    y++;
+ 
+    var loopTimer = setTimeout('move('+x+','+y+')',90);
 }
