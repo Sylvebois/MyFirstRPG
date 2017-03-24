0258c9848e47e85cfa6769cc24bf59da43a77e55
Sylvebois <thomasghaye@hotmail.com>
2017-03-24 14:44:00 +0100

    Moved mapTab to class Dungeon

--- public_html/js/classes/Dungeon.js
+++ public_html/js/classes/Dungeon.js
@@ -3,14 +3,32 @@
  */
 class Dungeon {
     constructor(){
-        
+        this.nbTilesPerLine = 20;
+        this.carte = [];
+        this.lvl = 0;
+        //Le contenu de la carte
+        /*
+            map[y][x] = {
+                'sol': 0;
+                'fog': 0;
+                'item': 0;
+                'monstre': 0;
+                'hero': 0;
+            }
+        */
     };
-    generate(onMap = false, mode = "default") {
-        if(onMap) {
-            
-        }
-        else {
-            console.log("il faut spécifier un tableau !");
+    generate(mode = 'default') {
+        for(let i = 0 ; i < nbTilesPerLine; i++) {
+            this.carte[i] = [];
+            for(let j = 0; j < nbTilesPerLine; j++) {
+                this.carte[i][j] = {
+                    'sol': new MapTile(i, j),
+                    'fog': 0,
+                    'item': false,
+                    'monstre': false,
+                    'hero': false
+                };
+            }
         }
     };
     addToMap(onMap) {
