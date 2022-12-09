8ab77c99f9569b3c890324cc06610fc2c42c2484
Sylvebois <thomasghaye@hotmail.com>
2022-12-09 21:49:52 +0000

    Translate the points left sentence

--- js/text.js
+++ js/text.js
@@ -54,6 +54,10 @@ const forms = {
     dx: { fr: "Dexterité : ", en: "Dexterity :" },
     iq: { fr: "Intellect : ", en: "Intellect : " },
     ht: { fr: "Santé : ", en: "Health : " },
+    pointsLeft: { 
+        fr: 'Il vous reste <span id="nbPoints">25</span> points à répartir.',
+        en: 'There is still <span id="nbPoints">25</span> points to dispatch.'
+    },
     music: { fr: "Musique : ", en: "Music : " },
     musicOn: { fr: "ON", en: "ON" },
     musicOff: { fr: "OFF", en: "OFF" },
