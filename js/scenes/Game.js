b0b3a5c45cd75fbbd47df0fe9078d1de5c892474
Sylvebois <thomasghaye@hotmail.com>
2023-01-10 15:04:42 +0000

    Started generation of levels and drawing them

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -1,6 +1,12 @@
+import { DungeonManager } from '../classes/LevelsManager.js';
+
 export default class Game {
     constructor(state) {
         this.tilesize = 32;
+        this.tileSizeOnScreen = 32;
+        this.mapSize = [20, 20];
+        this.dungeon = new DungeonManager(this.mapSize);
+
         this.gameInterface = document.getElementById('gameInterface');
         this.hud = document.getElementById('hud');
         this.gameZone = document.getElementById('gameZone');
@@ -19,7 +25,11 @@ export default class Game {
         buttons[0].addEventListener('click', e => this.goToMenu(state));
         buttons[1].addEventListener('click', e => this.goToInventory(state));
 
-        window.addEventListener('resize', e => this.setCanvasSize());
+        window.addEventListener('resize', e => {
+            this.setCanvasSize();
+            this.drawLvl(state.game.levels[state.game.player.level]);
+        });
+
         window.addEventListener('keydown', e => {
             if (state.currScene === 'gameInterface') {
                 if (e.key === 'ArrowUp') {
@@ -37,8 +47,8 @@ export default class Game {
                 else if (e.key === 'o' || e.key === 'O') { this.goToMenu(state); }
                 else if (e.key === 'i' || e.key === 'I') { this.goToInventory(state); }
             }
-            else if(state.currScene === 'inventory') {
-                if(e.key === 'Escape' || e.key === 'i' || e.key ==='I') { 
+            else if (state.currScene === 'inventory') {
+                if (e.key === 'Escape' || e.key === 'i' || e.key === 'I') {
                     this.goToGame(state);
                 }
             }
@@ -48,6 +58,7 @@ export default class Game {
     initCanvases() {
         let result = new Map();
         let size = Math.min(this.gameZone.offsetWidth, this.gameZone.offsetHeight);
+        this.tileSizeOnScreen = size / this.mapSize[0];
         let tags = document.getElementsByTagName('canvas');
 
         for (let elem of tags) {
@@ -61,7 +72,7 @@ export default class Game {
 
     setCanvasSize() {
         let size = Math.min(this.gameZone.offsetWidth, this.gameZone.offsetHeight);
-        let ratio = this.size / 720; //720 = default size of canvas
+        this.tileSizeOnScreen = size / this.mapSize[0];
         this.canvases.forEach(elem => {
             elem.can.setAttribute('width', size);
             elem.can.setAttribute('height', size);
@@ -74,6 +85,7 @@ export default class Game {
     }
 
     goToGame(state) {
+        state.gameIsRunning = true;
         state.currScene = 'gameInterface';
         this.gameInterface.style.visibility = 'visible';
     }
@@ -83,5 +95,58 @@ export default class Game {
         document.getElementById('menu').style.display = 'block';
         document.getElementById(state.currScene).style.display = 'block';
         this.gameInterface.style.visibility = 'hidden';
+    }
+
+    generateLvl(gameData) {
+        if (gameData.levels.length === 0) {
+            gameData.levels.push(this.dungeon.generateFirstLvl());
+        }
+    }
+
+    drawLvl(lvl) {
+        const back = this.canvases.get('background');
+        const img = this.images['newTileset'];
+
+        lvl.forEach((x, idx) => x.forEach((tile, idy) => {
+            back.context.drawImage(
+                img,
+                img.data[tile.type].x * img.data.tileSize,
+                img.data[tile.type].y * img.data.tileSize,
+                img.data.tileSize,
+                img.data.tileSize,
+                this.tileSizeOnScreen * idx,
+                this.tileSizeOnScreen * idy,
+                this.tileSizeOnScreen,
+                this.tileSizeOnScreen
+            );
+
+            if (tile.content.artefact) {
+                back.context.drawImage(
+                    img,
+                    img.data[tile.content.artefact].x * img.data.tileSize,
+                    img.data[tile.content.artefact].y * img.data.tileSize,
+                    img.data.tileSize,
+                    img.data.tileSize,
+                    this.tileSizeOnScreen * idx,
+                    this.tileSizeOnScreen * idy,
+                    this.tileSizeOnScreen,
+                    this.tileSizeOnScreen
+                );
+            }
+
+            if (tile.content.hero) {
+                back.context.drawImage(
+                    img,
+                    img.data['heroGoLeft'].x * img.data.tileSize,
+                    img.data['heroGoLeft'].y * img.data.tileSize,
+                    img.data.tileSize,
+                    img.data.tileSize,
+                    this.tileSizeOnScreen * idx,
+                    this.tileSizeOnScreen * idy,
+                    this.tileSizeOnScreen,
+                    this.tileSizeOnScreen
+                );
+            }
+        }))
     }
 }
