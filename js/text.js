327eda6f15934119b3283de85d223a3a31f617c2
Sylvebois <thomasghaye@hotmail.com>
2023-04-03 12:27:45 +0200

    Added translation + corrected typos

--- js/text.js
+++ js/text.js
@@ -8,32 +8,58 @@ const story = {
             `Vous vous ennuyez au bar du village, rêvant d'aventure au lieu de garder les poules quand vous entendez la conversation du prêtre avec le mage : 
 - ... Et c'est là que j'ai vu Marcel, le troll !
 - Et tu as fait quoi ?
-- Ben, j'ai pris mon courage à deux mains et j'ai courru le plus vite possible jusqu'au village. Mais j'ai perdu 2-3 bricoles en route et je suppose qu'il s'est servi ...
-- Ouais ... il faudrait pas que ça se sache dans le village, les nouvelles vont vites. 
+- Ben, j'ai pris mon courage à deux mains et j'ai couru le plus vite possible jusqu'au village. Mais j'ai perdu 2-3 bricoles en route et je suppose qu'il s'est servi ...
+- Ouais ... Il ne faudrait pas que ça se sache dans le village, les nouvelles vont vites. 
 - Je sais, j'y ai déjà pensé et je vais dire que j'ai créé un nouveau donjon rempli de trésors qui attend les aventuriers !
-- Bien vu, ça marche à tout les coups ces histoires de donjon, et puis ça relancera l'économie du village ...
+- Bien vu, ça marche à tous les coups ces histoires de donjon, et puis ça relancera l'économie du village ...
 - Et Marcel va en prendre pour son grade ! Allez, je t'offre une pinte pour fêter ça !
 
-C'est l'occasion rêvée de prouver à ces pecnots que vous avez l'étoffe d'un héro. Vous vous dirigez vers la grotte de Marcel, à l'entrée du village ...`,
+C'est l'occasion rêvée de prouver à ces pecnots que vous avez l'étoffe d'un héros. Vous vous dirigez vers la grotte de Marcel, à l'entrée du village ...`,
+        en:
+            `You are bored at the village bar, dreaming of adventure instead of tending the chickens when you overhear the priest's conversation with the mage: 
+- ... And that's when I saw Marcel, the troll !
+- And what did you do?
+- Well, I plucked up my courage and ran as fast as I could to the village. But I lost a few things on the way and I guess he took them...
+- Yeah ... it should not be known in the village, the news travels fast. 
+- I know, I've already thought about it and I'm going to say that I've created a new dungeon full of treasures that awaits adventurers!
+- Well done, it works every time these stories of dungeon, and then it will boost the economy of the village ...
+- And Marcel is going to be very sorry ! Come on, I offer you a pint to celebrate !
+
+It's the perfect opportunity to prove to these hicks that you have the right stuff to be a hero. You head towards Marcel's cave, at the entrance of the village...`
     },
     firstRun: {
         fr:
             `Quand la brume dans votre esprit se disperse, vous êtes chez vous, affalé sur le sol avec un goût de terre en bouche ... 
 Quand vous sortez voir le prêtre pour comprendre ce qui s'est passé, les gloussements et ricanements de vos concitoyens vous font dire que votre quête ne s'est pas déroulée comme prévu ...
 Le prêtre vous explique : "hum, Marcel a réussi à lire un parchemin de stupidité, il supprime toute l'intelligence de la cible. C'est un miracle que vous ayez pu rentrer chez vous. Il vous faudrait un casque de protection mais je l'ai ... heu ... égaré."
-De toute façon, le baraguinage des lanceurs de sort ressemble à du troll commun, alors un troll qui baraguine de la magie n'a rien d'étonnant selon vous.
+De toute façon, le baragouinage des lanceurs de sort ressemble à du troll commun, alors un troll qui baragouine de la magie n'a rien d'étonnant selon vous.
 
 Vous rassemblez ce qui vous reste de courage et d'honneur et vous retournez dans la caverne, décidé à mettre une raclée à Marcel ...`,
+        en:
+        `When the haze in your mind dissipates, you are at home, slumped on the floor with a taste of earth in your mouth... 
+When you go out to see the priest to find out what happened, the giggles and snickers of your fellow citizens make you realize that your quest did not turn out as planned...
+The priest explains to you: "um, Marcel managed to read a scroll of stupidity, it suppresses all the intelligence of the target. It's a miracle you were able to get home. You would need a protective helmet but I did... uh... lost it."
+Anyway, spellcaster chatter sounds like common troll, so a troll gabbling magic is not surprising in your opinion.
+
+You gather up what courage and honor you have left and head back to the cave, determined to beat the crap out of Marcel...`
     },
     final: {
         fr:
             `Vous rentrez au village, fier et victorieux !
 Vous avez vaincu Marcel, récupéré les bibelots du prêtre et pouvez enfin prouver que vous êtes un aventurier.
-Vous êtes accueilli en héro, enfin presque : des potes vous offre des bières, vous racontez votre aventure et d'autres vous racontent comment vous avez couvé un oeuf et picoré la terre avec vos poules quand vous êtiez abruti par le sort ...
-Le prêtre vous remercie et vous offre une belle besace d'or (en échange de votre silence sur sa conversation à la taverne) ...
-Finalement, la journée a été plutôt bonne et vous pouvez rêvez à d'autres aventures !
+Vous êtes accueilli en héros, enfin presque : des potes vous offrent des bières et vous écoutent raconter votre aventure ... Tandis que d'autres vous racontent comment vous avez couvé un oeuf et picoré la terre avec vos poules quand vous étiez abruti par le sort ...
+Le prêtre vous remercie et vous offre une belle besace d'or comme récompense (mais surtout en échange de votre silence à propos de sa conversation à la taverne) ...
+Finalement, la journée a été plutôt bonne et vous pouvez rêver à d'autres aventures !
 
-FIN`
+FIN`,
+        en:
+            `You return to the village, proud and victorious!
+You have defeated Marcel, recovered the priest's trinkets and can finally prove that you are an adventurer.
+You are welcomed as a hero, or almost: some friends offer you beers and listen to you telling your adventure... While others tell you how you hatched an egg and pecked the earth with your chickens when you were dazed by the spell...
+The priest thanks you and offers you a nice gold bag as a reward (but mostly in exchange of your silence about his conversation in the tavern) ...
+Finally, the day was rather good and you can dream of other adventures !
+            
+END`
     }
 };
 
