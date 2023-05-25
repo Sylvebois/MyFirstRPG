23913a7766f231b462bd9f1fd80e82b48ed77cd9
Sylvebois <thomasghaye@hotmail.com>
2023-05-25 14:09:30 +0200

    Updated tileset

--- js/imgMap.js
+++ js/imgMap.js
@@ -6,16 +6,16 @@ const newTileset = {
     heroGoDown: { x: 2, y: 0 },
     heroGoLeft: { x: 2, y: 0 },
     heroGoRight: { x: 3, y: 0 },
-    skeleton: { x: 4, y: 0 },
-    imp: { x: 5, y: 0 },
+    bat: { x: 4, y: 0 },
+    slime: { x: 5, y: 0 },
     helmet: { x: 0, y: 1 },
     sword: { x: 1, y: 1 },
     peek: { x: 2, y: 1 },
     flail: { x: 3, y: 1 },
     book: { x: 4, y: 1 },
     heart: { x: 5, y: 1 },
-    stairDown: { x: 1, y: 1 },
-    stairUp: { x: 0, y: 1 }
+    stairDown: { x: 1, y: 2 },
+    stairUp: { x: 0, y: 2 }
 }
 
 export { newTileset };
